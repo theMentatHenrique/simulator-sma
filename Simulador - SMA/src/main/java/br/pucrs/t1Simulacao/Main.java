51553588a676714864a912c5c1c2ba92bdf685cf
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2024-05-01 16:38:13 -0300

    Update Main.java

--- Simulador - SMA/src/main/java/br/pucrs/t1Simulacao/Main.java
+++ Simulador - SMA/src/main/java/br/pucrs/t1Simulacao/Main.java
@@ -1,7 +1,4 @@
 package br.pucrs.t1Simulacao;
-
-//Autores: Daniela Amaral e Vinicius Lima
-//video com execucao: https://youtu.be/bBtfh9CPhDU
 public class Main {
     public static void main(String[] args) {
         Simulator simulador = new Simulator();
